5f6ae47ef2f4a49edb73022fdbb17f26a10d856d
ZioCain <micael.castelleits@gmail.com>
2023-09-11 17:33:19 +0200

    Added small call for Filesystem

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,19 +1,13 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-  {
-    path: 'home',
-    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
-  },
-  {
-    path: 'message/:id',
-    loadComponent: () =>
-      import('./view-message/view-message.page').then((m) => m.ViewMessagePage),
-  },
-  {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full',
-  },
+	{
+		path: 'home',
+		loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
+	},
+	{
+		path: '',
+		redirectTo: 'home',
+		pathMatch: 'full',
+	},
 ];
-
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,28 +1,60 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject } from '@angular/core';
+import { Component } from '@angular/core';
+import { FileInfo, Filesystem, ReaddirResult } from '@capacitor/filesystem';
 import { IonicModule, RefresherCustomEvent } from '@ionic/angular';
-import { MessageComponent } from '../message/message.component';
-
-import { DataService, Message } from '../services/data.service';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
-  standalone: true,
-  imports: [IonicModule, CommonModule, MessageComponent],
+	selector: 'app-home',
+	templateUrl: 'home.page.html',
+	styleUrls: ['home.page.scss'],
+	standalone: true,
+	imports: [IonicModule, CommonModule],
 })
 export class HomePage {
-  private data = inject(DataService);
-  constructor() {}
+	folders: string[] = [];
+	currentPath:string = "/sdcard/"; // upmost readable path
+	constructor(){
+		// Upon start, show current directory listing
+		Filesystem.readdir({
+			path: this.currentPath
+		}).then((res:ReaddirResult)=>{
+			this.folders = [];
+			this.folders.push(...res.files.map((entry:FileInfo)=>{
+				return entry.name;
+			}));
+		});
+	}
 
-  refresh(ev: any) {
-    setTimeout(() => {
-      (ev as RefresherCustomEvent).detail.complete();
-    }, 3000);
-  }
+	/**
+	 * Refresh current view
+	 * @param ev The refresher event
+	 */
+	refresh(ev: any) {
+		Filesystem.readdir({
+			path: this.currentPath
+		}).then((res:ReaddirResult)=>{
+			this.folders = [];
+			this.folders.push(...res.files.map((entry:FileInfo)=>{
+				return entry.name;
+			}));
+			(ev as RefresherCustomEvent).detail.complete();
+		});
+	}
 
-  getMessages(): Message[] {
-    return this.data.getMessages();
-  }
+	Scan(folder:string){
+		if(folder === ".."){
+			this.currentPath = this.currentPath.replace(/\/.*\/$/, "/");
+			folder = "";
+		}
+		if(this.currentPath === "/") this.currentPath = "/sdcard/";
+		Filesystem.readdir({
+			path: this.currentPath + folder
+		}).then((res:ReaddirResult)=>{
+			// You'd expect the result "res" to contain both directories and files, but it keeps being directories only
+			this.folders = [".."];
+			this.folders.push(...res.files.map((entry:FileInfo)=>{
+				return entry.name;
+			}));
+		});
+	}
 }
